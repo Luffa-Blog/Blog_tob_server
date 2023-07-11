e6706887bd2f9ec9db6860a4d4fa88ddc7b159e6
dengzixu <sk8sigua@aliyun.com>
2023-07-11 17:10:31 +0800

    完成文章增删改

--- routes/articel.js
+++ routes/articel.js
@@ -1,38 +1,69 @@
-const { articelServer } = require('../service/articelServer')
-const { formatResponse } = require('../utils/tool')
-var express = require('express');
+const { articelServer, addServer, removeServer, updateServer } = require("../service/articelServer");
+const { formatResponse } = require("../utils/tool");
+var express = require("express");
 var router = express.Router();
-
-
-
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-// 获取文章接口
-
 // 查询
 router.post("/select", async (req, res) => {
+   const data = await articelServer(req.body);
 
-    const data = await articelServer(req.body)
-    console.log(data);
+   if (data.length === 0) {
+      return res.send(formatResponse((code = 201), "未查询到数据"));
+   }
+   if (data === "dateError") {
+      return res.send(formatResponse((code = 202), "时间戳为空或者时间戳不对"));
+   }
 
-    if (data.length === 0) {
-       return res.send(formatResponse(code = 201, "未查询到数据", ))
-    }
-    if (data==="dateError") {
-      return  res.send(formatResponse(code = 202, "时间戳为空或者时间戳不对", ))
-    }
+   return res.send(
+      formatResponse(
+         (code = 200),
+         "请求成功",
+         data.map((item) => item.dataValues)
+      )
+   );
+});
 
-   return  res.send(formatResponse(code = 200, "请求成功", data.map(item => item.dataValues)));
+// 新增
+router.post("/add", async (req, res) => {
+   const data = await addServer(req.body);
+   if (data === "dataError") {
+      return res.send(
+         formatResponse(201, "传输的数据有问题，请检查数据是否有空🈯️或未传")
+      );
+   }
 
+   if (data.dataValues) {
+      return res.send(formatResponse(200, "添加成功", data.dataValues));
+   }
+   returnres.send(formatResponse(202, "添加失败！"));
+});
 
+// 修改
+router.post("/update", async (req, res) => {
 
-})
+   const data = await updateServer(req.body)
 
+   if (data === "dataError") {
+      return res.send(
+         formatResponse(201, "失败,id可能有问题")
+      );
+   }
+   res.send(
+      formatResponse(200, "成功", data[0])
+   );
 
-router.post('/modify',async()=>{
+});
+// 删除
+router.post("/remove", async (req, res) => {
+   const data = await removeServer(req.body.id)
+   if (data === "dataError") {
+      return res.send(
+         formatResponse(201, "失败,id可能有问题")
+      );
+   }
+   res.send(
+      formatResponse(200, "成功", data)
+   );
 
-})
+});
 
 module.exports = router;
